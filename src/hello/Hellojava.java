1f4f92c17b51b29cd4cfcac95435a5d5224de93e
shimtt <sth7187@naver.com>
2025-02-13 10:35:13 +0900

    INIT

---
+++ src/hello/Hellojava.java
@@ -0,0 +1,16 @@
+package hello;
+/* 
+ * 주석 여러줄
+ * 작성자: 김 산
+ * 작성일: 25/02/12
+ * */
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// 반가워용
+		System.out.println("kim san, hi~");
+		
+	}
+
+}
